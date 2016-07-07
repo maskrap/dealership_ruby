117da9d1c9ebbd0da17c3d99df23cc37b53c1f68
Sam Park <sampark18@gmail.com>
2016-07-07 13:41:59 -0700

    Add erb necessary erb files such as success vehicle vehicleform

--- app.rb
+++ app.rb
@@ -6,3 +6,27 @@ require("./lib/vehicle")
 get("/") do
   erb(:index)
 end
+
+get('/vehicles') do
+  @vehicles = Vehicle.all()
+  erb(:vehicles)
+end
+
+get('/vehicles/new') do
+  erb(:vehicles_form)
+end
+
+post('vehicles') do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  erb(:success)
+end
+
+get('vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch("id"))
+  erb(:vehicle)
+end
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,7 +5,18 @@ class Vehicle
     @make = make
     @model = model
     @year = year
+    @id = @@vehicles.length().+(1)
   end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:find) do |identification|
+    found_vehicle = nil
+    @@vehicles.each() do |vehicle|
+      if vehicle.id().eql?(identification.to_i())
+          found_vehicle = vehicle
 
   define_method(:make)  do
     @make
